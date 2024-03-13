787731b6e548c71befb5fa7b38e28675ccd175e1
Shawn Rim <trim2@my.bcit.ca>
2024-03-12 18:40:56 -0700

    Cleanup main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,3 @@
-// firebase.auth().onAuthStateChanged((user) => {
-//     if (user) {
-//         //Do something
-//     } else {
-//         console.log("No user logged in.");
-//     }
-// });
-
-function bubbleSort(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr.length - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                const lesser = arr[j + 1];
-                arr[j + 1] = arr[j];
-                arr[j] = lesser;
-            }
-        }
-    }
-    // return the sorted array
-    return arr;
-}
-
 function sortArray(arr) {
     for (let i = 0; i < arr.length; i++) {
         for (let j = 0; j < arr.length - i - 1; j++) {

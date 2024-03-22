49a4019754627429335b8cc67068e1b17cc18b38
Shawn Rim <trim2@my.bcit.ca>
2024-03-21 20:16:15 -0700

    Save changes to the firebase when submit button is clicked

--- scripts/account.js
+++ scripts/account.js
@@ -2,6 +2,10 @@ const username = document.getElementById("username");
 const notificationOn = document.getElementById("notificationsOn");
 const notificationOff = document.getElementById("notificationsOff");
 const newsletter = document.getElementById("newsletter");
+const success = document.getElementById("writeSuccess");
+document.getElementById("closeBtn").addEventListener("click", () => {
+    success.classList.add("hidden");
+});
 
 function displayPersonalInfo() {
     firebase.auth().onAuthStateChanged(async (user) => {
@@ -24,4 +28,19 @@ function displayPersonalInfo() {
 
 displayPersonalInfo();
 
-function savePersonalInfo() {}
+function savePersonalInfo() {
+    firebase.auth().onAuthStateChanged(async (user) => {
+        if (user) {
+            await db.collection("users").doc(user.uid).set({
+                displayName: username.value,
+                notification: notificationOn.checked,
+                subscription: newsletter.checked,
+            });
+
+            displayPersonalInfo();
+            success.classList.remove("hidden");
+        } else {
+            console.log("No user logged in.");
+        }
+    });
+}

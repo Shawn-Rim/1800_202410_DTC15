202ef58afd43ce272fc51a647162545a7ed4491d
Shawn Rim <trim2@my.bcit.ca>
2024-03-27 20:15:21 -0700

    Protect the input fields by adding an edit button

--- scripts/account.js
+++ scripts/account.js
@@ -2,10 +2,25 @@ const username = document.getElementById("username");
 const notificationOn = document.getElementById("notificationsOn");
 const notificationOff = document.getElementById("notificationsOff");
 const newsletter = document.getElementById("newsletter");
+// const editBtn = document.getElementById("editBtn");
+// editBtn.addEventListener("click", enableEdit);
+const personalInfo = document.getElementById("personalInfoForm");
 const success = document.getElementById("writeSuccess");
 document.getElementById("closeBtn").addEventListener("click", () => {
     success.classList.add("hidden");
 });
+
+// const user = firebase.auth().currentUser;
+// const newPassword = getASecureRandomPassword();
+
+// user.updatePassword(newPassword)
+//     .then(() => {
+//         // Update successful.
+//     })
+//     .catch((error) => {
+//         // An error ocurred
+//         // ...
+//     });
 
 function displayPersonalInfo() {
     firebase.auth().onAuthStateChanged(async (user) => {
@@ -47,4 +62,16 @@ function savePersonalInfo() {
             console.log("No user logged in.");
         }
     });
+}
+
+function enableEdit() {
+    document.getElementById("editBtn").innerText = "Cancel";
+    document.getElementById("editBtn").onclick = disableEdit;
+    personalInfo.disabled = false;
+}
+
+function disableEdit() {
+    document.getElementById("editBtn").innerText = "Edit";
+    document.getElementById("editBtn").onclick = enableEdit;
+    personalInfo.disabled = true;
 }

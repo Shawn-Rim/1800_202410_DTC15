fe26a8bb473649d3f4d7516028ecfbbe4123241c
Alex Krantz <alex@krantz.dev>
2024-04-01 18:20:28 -0700

    Rewrite adding groceries to use ingredients collection

--- scripts/addGrocery.js
+++ scripts/addGrocery.js
@@ -4,17 +4,19 @@ function addGroceryItemForm() {
 
     let newTemplate = formTemplate.content.cloneNode(true);
 
+    newTemplate.querySelector("#itemName").addEventListener("keyup", ingredientSearch);
+
     formPlaceholder.appendChild(newTemplate);
 }
 
 function saveGroceryItems() {
     firebase.auth().onAuthStateChanged(async (user) => {
         if (user) {
-            let groceries = db.collection("users").doc(user.uid).collection("groceries");
-            let inputs = document.querySelectorAll(".formInput");
+            let groceriesRef = db.collection("users").doc(user.uid).collection("groceries");
+            let inputs = [...document.querySelectorAll(".formInput")];
 
-            inputs.forEach((form) => {
-                let name = form.querySelector("#itemName").value;
+            const groceries = inputs.map((form) => {
+                const item = form.querySelector("#itemName");
                 let quantity = form.querySelector("#quantity").value;
                 let unit = form.querySelector("#unit").value;
                 let expirationString = form.querySelector("#expirationDate").value;
@@ -24,21 +26,18 @@ function saveGroceryItems() {
                         : new Date(new Date(expirationString).getTime() + 86400000);
                 let cost = form.querySelector("#cost").value;
 
-                groceries
-                    .add({
-                        cost: cost * 100,
-                        createdAt: new Date(),
-                        expiration: expirationDate,
-                        name: name,
-                        quantity: quantity,
-                        unit: unit,
-                    })
-                    .then(() => {
-                        console.log("Grocery item added successfully");
-                    });
+                return groceriesRef.add({
+                    id: item.dataset.id,
+                    cost: cost * 100,
+                    createdAt: new Date(),
+                    expiration: expirationDate,
+                    name: item.value,
+                    quantity: quantity,
+                    unit: unit,
+                });
             });
 
-            await groceries.get();
+            await Promise.all(groceries);
             window.location.href = "/main.html";
         } else {
             console.log("No user logged in.");
@@ -48,4 +47,35 @@ function saveGroceryItems() {
 
 function removeForm(element) {
     $(element).parentsUntil("#formPlaceholder").remove();
+}
+
+async function ingredientSearch(event) {
+    let input = event.target.value.toLowerCase();
+    let div = event.target.nextElementSibling;
+    div.innerHTML = "";
+
+    let queries = await db
+        .collection("ingredients")
+        .where("name", ">", input)
+        .orderBy("name")
+        .limit(20)
+        .get();
+
+    let matches = new Map();
+    queries.forEach((doc) => {
+        if (doc.data().name.includes(input)) {
+            matches.set(doc.data().name, doc.id);
+        }
+    });
+
+    matches.forEach((id, ingredient) => {
+        const result = document.createElement("div");
+        result.innerText = ingredient;
+        result.addEventListener("click", () => {
+            event.target.value = ingredient;
+            event.target.dataset.id = id;
+        });
+
+        div.appendChild(result);
+    });
 }

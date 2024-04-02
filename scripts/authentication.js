3bc3a00096a2d0a4335502bd162aeba2056cebef
Alex Krantz <alex@krantz.dev>
2024-04-02 11:29:13 -0700

    Create new user document on sign up

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -1,10 +1,17 @@
 const requiredState = document.currentScript.dataset.require;
+const once = document.currentScript.dataset.once === "true";
+
+let triggered = false;
 firebase.auth().onAuthStateChanged((user) => {
+    if (triggered && once) return;
+
     const state = user ? "authenticated" : "unauthenticated";
     if (state !== requiredState) {
         const page = pageForState(state);
         window.location.href = page;
     }
+
+    triggered = true;
 });
 
 /**
--- scripts/login.js
+++ scripts/login.js
@@ -2,13 +2,41 @@ const ui = new firebaseui.auth.AuthUI(firebase.auth());
 
 ui.start("#firebase-auth", {
     callbacks: {
-        signInSuccessWithAuthResult: () => true,
+        signInSuccessWithAuthResult,
         uiShown: () => $("#loader").hide(),
     },
     signInFlow: "popup",
-    signInSuccessUrl: "app.html",
     signInOptions: [
         firebase.auth.EmailAuthProvider.PROVIDER_ID,
         firebase.auth.GoogleAuthProvider.PROVIDER_ID,
     ],
 });
+
+/**
+ * Successful sign in callback
+ *
+ * @param {firebaseui.auth.AuthResult} result the result of a sign in
+ * @returns {boolean} whether to redirect automatically
+ */
+function signInSuccessWithAuthResult(result) {
+    console.log(JSON.stringify(result, null, 2));
+    if (!result.additionalUserInfo.isNewUser) return true;
+
+    const user = result.user;
+    db.collection("users")
+        .doc(user.uid)
+        .set({
+            displayName: user.displayName,
+            email: user.email,
+            notification: true,
+            subscription: true,
+            favouriteRecipes: [],
+        })
+        .then(() => {
+            console.log("User created successfully");
+            window.location.href = "/app.html";
+        })
+        .catch((err) => console.error(`Failed to create user: ${err}\n${err.stack}`));
+
+    return false;
+}

7689923d2c2badb402a685791b5c908a545c7ae5
Alex Krantz <alex@krantz.dev>
2024-04-09 08:44:53 -0700

    Make template compatible with nested folders

--- scripts/components.js
+++ scripts/components.js
@@ -1,6 +1,6 @@
-$(".component").each((_index, component) => $(component).load(`components/${component.id}.html`));
+$(".component").each((_index, component) => $(component).load(`/components/${component.id}.html`));
 
 firebase.auth().onAuthStateChanged((user) => {
     const state = user ? "authenticated" : "unauthenticated";
-    $("#navbar").load(`components/navbar/${state}.html`);
+    $("#navbar").load(`/components/navbar/${state}.html`);
 });
